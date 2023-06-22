5f06ccea6d195ccf3c7c73fb15eaef72c11bb84d
raghavrrrao <79044743+raghavrrrao@users.noreply.github.com>
2023-06-22 13:30:05 +0530

    Add files via upload

---
+++ MPHtoMPSconvertor.py
@@ -0,0 +1,5 @@
+print("Welcome to the MPH to MPS conversion app")
+mph = float(input("What is your speed in miles per hour: "))
+mps = mph*0.4474
+mps = round(mps, 2)
+print("your speed in MPS is " + str(mps) + "." )
